10745cbc20b1943cf4df7b2c037964259f9ff4f2
anirusn <neupane.arjun8@gmail.com>
2024-04-20 20:05:28 -0400

    Update vm_user_data.sh

--- vm_user_data.sh
+++ vm_user_data.sh
@@ -13,16 +13,8 @@ sudo yum install unzip -y
 
 sudo yum install git -y
 
-# Download the zip file from S3
-#sudo git clone https://github.com/anirusn/staticweb.git /var/www/html/
-
 sudo echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Group 16</title><style>body { font-family: Arial, sans-serif; margin: 0; padding: 0; background: linear-gradient(135deg, #3498db, #2ecc71); min-height: 100vh; display: flex; justify-content: center; align-items: center; } .container { max-width: 800px; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); } h1 { text-align: center; color: #fff; } h2 { text-align: center; color: #fff; } .group-info { text-align: center; margin-top: 20px; color: #333; } img { display: block; margin: 20px auto; max-width: 100%; height: auto; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); }</style></head><body><div ><h1>Group 16</h1><img src="https://fpbucket730.s3.amazonaws.com/sunrise.jpg" alt="Group 16 Image"><div class="group-info"><h2>Group Member: Arjun Neupane</h2></div></div></body></html>
 ' > /var/www/html/index.html
-
-# Unzip the file
-#sudo unzip /var/www/html/fp.zip -d /var/www/html/
-
-#sudo mv /var/www/html/staticweb* /var/www/html/
 
 # Ensure proper permissions
 sudo chmod 755 /var/www/html/
